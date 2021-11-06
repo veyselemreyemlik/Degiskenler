0f5ee835180ef6b83cdd9022a83b926683524c77
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2021-11-06 18:16:20 +0300

    Add files via upload

---
+++ degıskenler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace degıskenler
+{
+    class Program
+    {
+    
+
+        static void Main(string[] args)
+        {
+            string v = "Tarih ve Saat :";
+            Console.WriteLine (v);
+            
+            DateTime dt = DateTime.Now;
+            Console.WriteLine (dt);
+
+            string str = "20";
+            int rakam = 20;
+
+            string yeniDeger = str + rakam.ToString();
+            Console.WriteLine (yeniDeger);
+            
+            int newDeger  = rakam + Convert.ToInt32(str);
+
+             Console.WriteLine (newDeger);
+            
+
+        }
+    }
+}
